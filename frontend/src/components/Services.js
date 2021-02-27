255c0f0e3dc18861f6e1167fd117e760f861336b
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-02-27 23:45:23 +0800

    removed lorem in services

--- frontend/src/components/Services.js
+++ frontend/src/components/Services.js
@@ -48,7 +48,7 @@ const Services = () => {
                     <div className="row">
                         <div className="col-lg-12 text-center">
                             <h2 className="section-heading">Our Services </h2>
-                            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur. </h3>
+                           
                         </div>
                     </div>
 
@@ -56,11 +56,11 @@ const Services = () => {
                     <div className="row text-center">
                         <div className="col-md-6"><span className="fa-stack fa-4x"><i className="fa fa-circle fa-stack-2x text-primary"></i><i className={`fa ${netSecIcon} fa-stack-1x fa-inverse`}></i> </span>
                             <h4 className="service-heading">{networkSecurity}</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit. </p>
+                            
                         </div>
                         <div className="col-md-6"><span className="fa-stack fa-4x"><i className="fa fa-circle fa-stack-2x text-primary"></i><i className={`fa ${webDevIcon} fa-stack-1x fa-inverse`}></i> </span>
                             <h4 className="service-heading">{websiteDevelopment}</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit. </p>
+                            
                         </div>
                     </div>
 
@@ -68,11 +68,11 @@ const Services = () => {
                     <div className="row text-center">
                         <div className="col-md-6"><span className="fa-stack fa-4x"><i className="fa fa-circle fa-stack-2x text-primary"></i><i className={`fa ${battTestIcon} fa-stack-1x fa-inverse`}></i> </span>
                             <h4 className="service-heading">{batteryTestingServices}</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit. </p>
+                            
                         </div>
                         <div className="col-md-6"><span className="fa-stack fa-4x"><i className="fa fa-circle fa-stack-2x text-primary"></i><i className={`fa ${partDiscIcon} fa-stack-1x fa-inverse`}></i> </span>
                             <h4 className="service-heading">{partialDischargeDetection}</h4>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit. </p>
+                            
                         </div>
                     </div>
                 </div>
