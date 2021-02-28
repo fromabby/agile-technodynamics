9f2f15853e3a14fdaa69a063f4887ff8c8c231cb
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-02-28 21:17:09 +0800

    added default for description

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose')
 
+const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear(); 
+    const hrs = String(today.getHours()).padStart(2,'0');
+    const minutes = String(today.getMinutes()).padStart(2,'0');
+    const todayDate = mm + '/' + dd + '/' + yyyy;
+    const todayTime = hrs +':'+ minutes;
+
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -10,6 +19,7 @@ const productSchema = new mongoose.Schema({
     description: {
         type: String,
         required: [true, 'Please enter product description'],
+        default: `Created at ${todayDate} ${todayTime}`
     },
     images: [
         {
