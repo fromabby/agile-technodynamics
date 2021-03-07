bf2e91e45a54142688093c1ddcdf32bea5e8c411
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-03-07 23:02:08 +0800

    Initial Commit

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,17 @@ const cookieParser = require('cookie-parser');
 const bodyparser = require('body-parser')
 const fileUpload = require('express-fileupload')
 const errorMiddleware = require('./middlewares/errors')
+//const dotenv = require('dotenv');
+const path = require('path')
 
 app.use(express.json());
 app.use(bodyparser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(fileUpload());
+
+
+// Setting up config file
+if(process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
   
 //Import all the routes
 const products = require('./routes/product');
@@ -27,6 +33,14 @@ app.use('/api/v1/', home)
 app.use('/api/v1/', about)
 app.use('/api/v1/', footerInfo)
 app.use('/api/v1', services)
+
+if(process.env.NODE_ENV === 'PRODUCTION'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 
 //Middleware to handle errors
