44048aec228e56c97fc48c6c0140e26857f0d738
fromabby <60252891+fromabby@users.noreply.github.com>
2021-03-24 01:10:06 +0800

    Update Products.js

--- frontend/src/components/product/Products.js
+++ frontend/src/components/product/Products.js
@@ -99,7 +99,7 @@ const Products = () => {
                         <p className="text-center product-category" style={{color: '#0d163f'}}>
                             <a onClick={() => {
                                 setCurrentPageNo(1) 
-                                setMainCategory('Mechanical Engineering')
+                                setMainCategory('')
                                 setSubCategory('')
                                 }
                             }>{category}</a>
