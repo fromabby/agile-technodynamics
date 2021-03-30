a7cfd75008b19814a44d188cfca4d71074ac12e5
miguelybera <josemiguel.ybera.iics@ust.edu.ph>
2021-03-30 18:13:08 +0800

    returned user id in product

--- backend/models/product.js
+++ backend/models/product.js
@@ -65,6 +65,11 @@ const productSchema = new mongoose.Schema({
             ]
         }
     },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: true
+    }
 })
 
 module.exports = mongoose.model('Product', productSchema)
